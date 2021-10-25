dc73e2460a155ada03985d3c1d450b887df6349e
André Rossner <rossnerandre@gmail.com>
2021-10-25 11:58:39 -0300

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
+
 
 const App = () => {
   const expenses = [
     { id: 'e1', title: 'Toilet Paper', amount: 94.75, date: new Date(2021, 7, 14) },
     { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 8, 5) }
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In app.js');
+    console.log(expense);
+  };
+
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
