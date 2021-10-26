59cd8c0ab23e4839727a18ca0b682ac46b28a24f
André Rossner <rossnerandre@gmail.com>
2021-10-26 10:05:41 -0300

    Add ExpenseList Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 import Card from '../UI/Card';
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -16,24 +16,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date} />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    handleEditing()
+  }
+
+  const handleEditing = () => {
+    setIsEditing(!isEditing);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing ?
+        <button
+          onClick={handleEditing}>
+          Add New Expense </button> :
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onEditing={handleEditing} />
+      }
     </div>
   );
 }
